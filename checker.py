3fe08cd3e63e1f76769c5b47b351ff1c86f1a1e8
jihye <jhlov.jin@samsung.com>
2024-06-13 15:52:08 +0900

    [feature] length_same

--- checker.py
+++ checker.py
@@ -1,3 +1,10 @@
 
 class Checker():
-    pass
+    def __init__(self):
+        self.length_similarity = 0
+
+    def get_length_similarity(self):
+        return self.length_similarity
+    def update_length_similarity(self, str1, str2):
+        if str1 == str2:
+            self.length_similarity = 60
--- test_checker.py
+++ test_checker.py
@@ -2,5 +2,8 @@ from unittest import TestCase
 from checker import Checker
 
 class TestChecker(TestCase):
-    def test(self):
+    def test_length_same(self):
+        checker = Checker()
+        checker.update_length_similarity("123", "123")
+        self.assertEqual(checker.get_length_similarity(), 60)
         pass
